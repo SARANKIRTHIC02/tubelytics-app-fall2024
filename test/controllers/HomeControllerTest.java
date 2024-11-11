c2523960b394bcfd4d29f26ada11660acce9197c
Duraipaandiyaan A P <52820420+sankardurai17@users.noreply.github.com>
2024-11-10 21:59:51 -0500

    Home Controller Test update

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import model.ChannelProfileResult;
 import model.TubelyticService;
 import model.VideoSearchResult;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import java.util.*;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mockStatic;
+import static play.mvc.Http.Status.NOT_FOUND;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.route;
@@ -28,7 +30,7 @@ public class HomeControllerTest extends WithApplication {
     private HomeController homeController;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         MockitoAnnotations.openMocks(this);
     }
 
@@ -61,15 +63,14 @@ public class HomeControllerTest extends WithApplication {
 
             Result result = route(app, request);
 
-            // Assert: Check status and response content
             assertEquals(OK, result.status());
             String responseContent = contentAsString(result);
-            assertTrue(responseContent.contains("<h2>Search Results for term: sampleQuery</h2>")); // Adjust based on actual response format
+            assertTrue(responseContent.contains("<h2>Search Results for term: sampleQuery</h2>"));
         }
     }
 
     @Test
-    public void testTaglyticsWithQuery_MockedResponse() throws IOException, InterruptedException {
+    public void testTaglyticsWithQuery() throws IOException, InterruptedException {
         System.out.println("HomeController 3");
         String query = "SampleVideo";
         List<VideoSearchResult> mockResults = new ArrayList<>();
@@ -87,18 +88,21 @@ public class HomeControllerTest extends WithApplication {
             Result result = route(app, request);
             assertEquals(OK, result.status());
             String responseContent = contentAsString(result);
-            assertTrue(responseContent.contains("<h2>Search Results for term: SampleVideo</h2>")); // Adjust based on actual response format
+            assertTrue(responseContent.contains("<h2>Search Results for term: SampleVideo</h2>"));
 
         }
     }
 
-   /* @Test
-    public void testWordStatsWithSearchQuery() {
+    @Test
+    public void testWordStatsWithSearchQuery() throws IOException, InterruptedException {
         String searchQuery = "sampleSearch";
-        String mockWordStatsData = "Mocked word stats data";
+        List<VideoSearchResult> mockResults = new ArrayList<>();
+        mockResults.add(new VideoSearchResult("video123", "Sample Video", "Sample description", "thumbnail", "channel123", "Sample Channel", null));
+        Map<String, Long> mockWordStats = Map.of("sample", 2L, "description", 1L);
 
-        try (MockedStatic<YouTubeService> mockedService = mockStatic(YouTubeService.class)) {
-            mockedService.when(() -> YouTubeService.fetchWordStats(searchQuery)).thenReturn(mockWordStatsData);
+        try (MockedStatic<TubelyticService> mockedService = mockStatic(TubelyticService.class)) {
+            mockedService.when(() -> TubelyticService.fetchResults(searchQuery)).thenReturn(mockResults);
+            mockedService.when(() -> TubelyticService.wordStatistics(mockResults)).thenReturn(mockWordStats);
 
             Http.RequestBuilder request = new Http.RequestBuilder()
                     .method(GET)
@@ -106,26 +110,71 @@ public class HomeControllerTest extends WithApplication {
 
             Result result = route(app, request);
             assertEquals(OK, result.status());
-            assertEquals("Mocked word stats data", contentAsString(result));
+            String responseContent = contentAsString(result);
+            assertTrue(responseContent.contains("<thead>\n" +
+                    "            <tr>\n" +
+                    "                <th>Word</th>\n" +
+                    "                <th>Frequency</th>\n" +
+                    "            </tr>\n" +
+                    "            </thead>"));
+        }
+    }
+    @Test
+    public void testTags() throws IOException, InterruptedException {
+        String videoID = "video123";
+        List<VideoSearchResult> mockResults = new ArrayList<>();
+        mockResults.add(new VideoSearchResult("video123", "Sample Video", "Sample description", "thumbnail", "channel123", "Sample Channel", List.of("tag1", "tag2", "tag3")));
+
+        try (MockedStatic<TubelyticService> mockedService = mockStatic(TubelyticService.class)) {
+            mockedService.when(() -> TubelyticService.fetchResults(videoID)).thenReturn(mockResults);
+
+            Http.RequestBuilder request = new Http.RequestBuilder()
+                    .method(GET)
+                    .uri("/ytlytics/tags/" + videoID);
+
+            Result result = route(app, request);
+            assertEquals(OK, result.status());
         }
     }
 
     @Test
-    public void testWordStatsWithoutSearchQuery() {
-        String mockWordStatsData = "Mocked empty word stats data";
+    public void testChannelVideosFound() {
+        String channelID="UCi7Zk9baY1tvdlgxIML8MXg";
+        ChannelProfileResult mockChannelProfile=new ChannelProfileResult(channelID, "AVC News","",200000L,"","",Collections.emptyList());
 
-        try (MockedStatic<YouTubeService> mockedService = mockStatic(YouTubeService.class)) {
-            mockedService.when(() -> YouTubeService.fetchWordStats("")).thenReturn(mockWordStatsData);
+
+        try (MockedStatic<TubelyticService> mockedService = mockStatic(TubelyticService.class)) {
+            mockedService.when(() -> TubelyticService.fetchChannelDetails(channelID)).thenReturn(mockChannelProfile);
 
             Http.RequestBuilder request = new Http.RequestBuilder()
                     .method(GET)
-                    .uri("/wordStatistics");
+                    .uri("/ytlytics/channel/" + channelID);
 
             Result result = route(app, request);
             assertEquals(OK, result.status());
-            assertEquals("Mocked empty word stats data", contentAsString(result));
+            String responseContent = contentAsString(result);
+            System.out.println(responseContent);
+            assertTrue(responseContent.contains("<h1 id=\"channelName\">Channel Name: CTV News</h1>"));
         }
-    }*/
+    }
+
+    @Test
+    public void testChannelVideosNotFound() {
+        String channelId = "nonExistentChannel";
+
+        try (MockedStatic<TubelyticService> mockedService = mockStatic(TubelyticService.class)) {
+            mockedService.when(() -> TubelyticService.fetchChannelDetails(channelId)).thenReturn(null);
+
+            Http.RequestBuilder request = new Http.RequestBuilder()
+                    .method(GET)
+                    .uri("/ytlytics/channel/" + channelId);
+
+            Result result = route(app, request);
+            assertEquals(NOT_FOUND, result.status());
+            String responseContent = contentAsString(result);
+            assertEquals("Channel not found with ID: " + channelId, responseContent);
+        }
+    }
 }
 
 
