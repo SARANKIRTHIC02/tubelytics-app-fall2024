8e8f3186dac66148ccab12f4e3d9d62c56632515
Sushanth Ravishankar <sushanthpvr@gmail.com>
2024-12-01 15:04:31 -0500

    updated TagActorTest.java
    mocked

--- test/actors/TagActorTest.java
+++ test/actors/TagActorTest.java
@@ -1,9 +1,7 @@
 package actors;
 
-import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
 import akka.testkit.javadsl.TestKit;
 import model.TubelyticService;
 import model.VideoSearchResult;
@@ -15,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class TagActorTest {
     private static ActorSystem system;
@@ -32,8 +29,7 @@ public class TagActorTest {
     }
 
     @Test
-    public void testHandleValidTag() {
-        // Prepare mock data
+    public void testHandleValidTagWithoutAPI() {
         String testTag = "technology";
         List<VideoSearchResult> mockResults = new ArrayList<>();
         mockResults.add(new VideoSearchResult("video1", "Video Title 1", "Description 1", "thumbnail1", "channel1", "Channel 1", null));
@@ -49,26 +45,52 @@ public class TagActorTest {
     }
 
     @Test
-    public void testHandleExceptionDuringFetch() {
-        String testTag = "invalid";
-
+    public void testHandleEmptyTagWithoutAPI() {
+        String testTag = "";
+        List<VideoSearchResult> mockResults = new ArrayList<>(); // Empty list for empty tag
         TubelyticService tubelyticServiceMock = mock(TubelyticService.class);
-        when(TubelyticService.fetchResults(testTag)).thenThrow(new RuntimeException("Mocked exception"));
-
+        when(TubelyticService.fetchResults(testTag)).thenReturn(mockResults);
         new TestKit(system) {{
             final ActorRef tagActor = system.actorOf(TagActor.props());
             tagActor.tell(testTag, getRef());
-            expectMsgClass(akka.actor.Status.Failure.class);
+            expectMsg(mockResults);
             verify(tubelyticServiceMock, times(1)).fetchResults(testTag);
         }};
     }
 
     @Test
-    public void testHandleUnsupportedMessage() {
+    public void testHandleInvalidCharactersWithoutAPI() {
+        String invalidTag = "!@#$%^&*";
+        TubelyticService tubelyticServiceMock = mock(TubelyticService.class);
+        when(TubelyticService.fetchResults(invalidTag)).thenThrow(new RuntimeException("Invalid input"));
         new TestKit(system) {{
             final ActorRef tagActor = system.actorOf(TagActor.props());
-            tagActor.tell(12345, getRef());
+            tagActor.tell(invalidTag, getRef());
             expectMsgClass(akka.actor.Status.Failure.class);
+            verify(tubelyticServiceMock, times(1)).fetchResults(invalidTag);
+        }};
+    }
+
+    @Test
+    public void testHandleConcurrencyWithoutAPI() {
+        // Prepare mock data
+        String tag1 = "tag1";
+        String tag2 = "tag2";
+        List<VideoSearchResult> results1 = new ArrayList<>();
+        results1.add(new VideoSearchResult("video1", "Title1", "Desc1", "thumb1", "channel1", "Channel1", null));
+        List<VideoSearchResult> results2 = new ArrayList<>();
+        results2.add(new VideoSearchResult("video2", "Title2", "Desc2", "thumb2", "channel2", "Channel2", null));
+        TubelyticService tubelyticServiceMock = mock(TubelyticService.class);
+        when(TubelyticService.fetchResults(tag1)).thenReturn(results1);
+        when(TubelyticService.fetchResults(tag2)).thenReturn(results2);
+        new TestKit(system) {{
+            final ActorRef tagActor = system.actorOf(TagActor.props());
+            tagActor.tell(tag1, getRef());
+            tagActor.tell(tag2, getRef());
+            expectMsg(results1);
+            expectMsg(results2);
+            verify(tubelyticServiceMock, times(1)).fetchResults(tag1);
+            verify(tubelyticServiceMock, times(1)).fetchResults(tag2);
         }};
     }
 }
